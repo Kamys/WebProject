124bbbf24e65c4a6690643246bb102cdf764c241
Nikita Kamyshenko <hnkntoc@gmail.com>
2017-07-11 22:29:38 +0300

    Добавлена главная страница

---
+++ WebProject/Controllers/ProductController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProject.Models;
+
+namespace WebProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Goods
+        public ActionResult Index()
+        {
+            IList<Product> products = new List<Product>()
+            {
+                new Product() {Name = "Product 1"},
+                new Product() {Name = "Product 2"},
+                new Product() {Name = "Product 3"},
+            };
+            return View();
+        }
+
+        // GET: Goods/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Goods/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Goods/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Goods/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Goods/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Goods/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Goods/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebProject/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+    }
+}
